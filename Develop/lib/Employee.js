f486eb5674cd43be69e0b8f58b204e0656b1c919
Maria Jimena <mja279@georgetown.edu>
2020-08-18 18:54:46 -0400

    initial commit

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,10 @@
+// TODO: Write code to define and export the Employee class
+const = 
+
+class Employee {
+    constructor () {
+        this.name = name;
+        this.id = 0;
+        this.email = email;
+    }
+}
